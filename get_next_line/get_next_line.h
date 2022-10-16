5f7e556f8d2974aaabc26434994ab24959b61e02
muhammadganiev <mukhammad.ganiev@mail.ru>
2022-10-16 20:05:04 +0400

    gnl +

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,13 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: muganiev <muganiev@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/05 18:09:14 by muganiev          #+#    #+#             */
-/*   Updated: 2022/10/16 18:22:14 by muganiev         ###   ########.fr       */
+/*   Created: 2022/10/16 19:58:46 by muganiev          #+#    #+#             */
+/*   Updated: 2022/10/16 20:03:55 by muganiev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000000
+# endif
+
+char	*get_next_line(int fd);
+int		gnl_strlen(char *s);
+char	*gnl_strchr(char *s);
+char	*gnl_strdup(char	*remainder);
+char	*gnl_strjoin(char *old, char *new);
+
+#endif
--- solong.h
+++ solong.h
@@ -6,23 +6,80 @@
 /*   By: muganiev <muganiev@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 16:43:12 by muganiev          #+#    #+#             */
-/*   Updated: 2022/10/16 17:02:52 by muganiev         ###   ########.fr       */
+/*   Updated: 2022/10/16 18:21:59 by muganiev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- #ifndef SOLONG_H
- # define SOLONG_H
- 
- # include <stdio.h>
- # include <stdlib.h>
- # include <unistd.h>
- # include "mlx/mlx.h"
- # include "gnl/get_next_line.h"
+ #ifndef SO_LONG_H
+# define SO_LONG_H
 
- typedef	s_prog;
- void		*mlx;
- void		*window;
- int		size_x;
- int		size_y;
- t_map		map;
- #endif
+# include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
+# include "minilibx/mlx.h"
+# include <fcntl.h>
+
+# define GROUND		"img/ground.xpm"
+# define WALL		"img/wall.xpm"
+# define PLAYER1	"img/player1.xpm"
+# define PLAYER2	"img/player2.xpm"
+# define KEY		"img/key.xpm"
+# define EXIT		"img/exit.xpm"
+# define DANGER		"img/danger.xpm"
+
+typedef struct s_game
+{
+	int		w;
+	int		h;
+	char	**map;
+	int		collectible_item;
+	int		flag;
+	char	hash_map[256][256];
+	int		find_e;
+	int		find_c;
+	int		ppx;
+	int		ppy;
+	int		steps;
+	void	*mlx;
+	void	*mlx_win;
+	void	*img;
+}	t_game;
+
+void	ft_error(char *string);
+void	ft_error_tempmap(char *tempmap, char *string);
+void	ft_error_map(t_game *game, char *string);
+
+char	*get_next_line(int fd);
+int		gnl_strlen(char *s);
+char	*gnl_strchr(char *s);
+char	*gnl_strdup(char *remainder);
+char	*gnl_strjoin(char *old, char *new);
+
+void	ft_parsing(t_game *game, char *file);
+void	ft_check_file_extension(char *file);
+char	*ft_read_map(char *file);
+void	ft_check_map(t_game *game, char *read_map);
+void	ft_check_boundaries(t_game *game);
+void	ft_check_number_elements(t_game *game);
+int		ft_count_elements(t_game *game, char c);
+void	ft_check_simbols(t_game *game);
+
+void	ft_render_map(t_game *game);
+void	ft_render_window(t_game *game);
+void	ft_put_image_and_find_player(t_game *game, char c, int x, int y);
+void	ft_put_image2(t_game *game, char c, int x, int y);
+void	ft_render_image(t_game *game, char *path, int x, int y);
+
+void	ft_valid_path_to_exit(t_game *game);
+void	ft_scan_map(t_game *game, int x, int y);
+int		ft_check_path_to_exit(t_game *game, int x, int y);
+
+int		ft_player_movement(int key, t_game *game);
+void	ft_next_step(t_game *game, int x, int y);
+void	ft_flag0_steps(t_game *game, int next_x, int next_y);
+void	ft_flag1_steps(t_game *game, int next_x, int next_y);
+void	ft_met_exit(t_game *game, int next_x, int next_y);
+void	ft_render_move(t_game *game, int next_x, int next_y);
+void	ft_if_flag2_flag3_render(t_game *game, int next_x, int next_y);
+int		ft_close_window(t_game *game);
+
+#endif
